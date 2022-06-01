b2e24c319b49948b4ef19f21dad45c87b3702be7
Maksence <102806093+Maksence@users.noreply.github.com>
2022-06-01 11:14:28 +0200

    Update naivecluster.py

--- naivecluster.py
+++ naivecluster.py
@@ -5,8 +5,10 @@ from scipy.spatial import distance
 from matplotlib import pyplot as plt
 import geopy.distance
 from math import sin, cos, sqrt, atan2, radians
+import time
+import csv
 data = pd.read_csv("generated.csv")
-
+start_time = time.time()
 #print(data.head()) # head() method returns top n rows of a DataFrame  with n=5 is default
 
 #data.plot.scatter(x='LON',y='LAT') #Points sur le graphe
@@ -32,25 +34,24 @@ for i in range(len(lon)):
 liste_copie = liste_points.copy()
 print(len(liste_copie))
 print(len(liste_points))
+longueur = len(liste_copie)
 #print(liste_copie[1])
 #print(liste_copie[0])
 
 #Distance entre 2 points
-def dist(new_cluster,i,liste):
+def dist2(new_cluster,i,liste):
     R = 6373.0
-
     lat1 = radians(new_cluster[1])
     lon1 = radians(new_cluster[0])
     lat2 = radians(liste[i][1])
     lon2 = radians(liste[i][0])
-
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = R * c  
-    print(distance)  
+    #print(distance)  
     return distance
 
 def dist3(new_cluster,i,liste):
@@ -60,7 +61,7 @@ def dist3(new_cluster,i,liste):
     #print(p1,p2)
     return geopy.distance.geodesic(p1,p2).km
 
-def dist2(centre_cluster,i,liste):
+def dist(centre_cluster,i,liste):
     p1=[centre_cluster[0],centre_cluster[1]]
     p2=[liste[i][0],liste[i][1]]
     #print(p1)
@@ -69,6 +70,7 @@ def dist2(centre_cluster,i,liste):
     #print("distande entre les 2 points")
     #print(dst)
     return dst
+
 #print(dist(1,2))
 nb_clusters = 0
 #Début de l'algo
@@ -81,52 +83,36 @@ while (len(liste_copie)>0): #Tant que la liste n'est pas vide, cad que tous les 
     nb_clusters += 1
     del liste_copie[0] 
     nb_supprime += 1
-    while ((count<len(liste_copie)) and debit<debit_MAX): #Pour chaque cluster, on cherche à le remplir au maximum. La seconde condition est pour éviter de parcourir inutilement
+    while ((count<len(liste_copie)) and debit+50<debit_MAX): #Pour chaque cluster, on cherche à le remplir au maximum. La seconde condition est pour éviter de parcourir inutilement
         #print(count)
         #print(len(liste_copie))
         #print(nb_supprime)
-        print("Longueur de la liste: ", len(liste_copie))
-        print("Count: ", count)
-        print("debit : ", debit)
-        if (dist(new_cluster[0],count-nb_supprime,liste_copie)<45 and (debit+liste_copie[count-nb_supprime][2]<debit_MAX)):
+        #print("Longueur de la liste: ", len(liste_copie))
+        #print("Count: ", count)
+        #print("debit : ", debit)
+        if (dist2(new_cluster[0],count-nb_supprime,liste_copie)<45 and (debit+liste_copie[count-nb_supprime][2]<debit_MAX)):
             debit += liste_copie[count-nb_supprime][2]
             new_point_cluster = liste_copie[count-nb_supprime]
             new_cluster.append(new_point_cluster)
-            print("nb_supprimé ",nb_supprime)
+            #print("nb_supprimé ",nb_supprime)
             del liste_copie[count-nb_supprime]
             nb_supprime+=1
         count += 1
     clusters.append(new_cluster)
+    #print(new_cluster)
+    pourcentage = (longueur-count)/longueur
+    print("Count = ", count, "Nb_suppr = ", nb_supprime, "debit du cluster = ", debit, "longueur des centres =", len(clusters),)
+    print( "Pourcentage de complétion: ", pourcentage*100,"%", "--- %s seconds ---" % (time.time() - start_time))
+    
+#print(clusters)
+#print("nb_cluster : ", nb_clusters)
+print("Nombre de clusters ",len(clusters[0]))
 
-""""   
-while (len(liste_copie)>0): #Tant que la liste n'est pas vide
+with open('NaiveCSV', 'w') as f:
+      
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+    write.writerow(clusters)
 
-    centres.append(liste_copie[0]) #On initialise/choisis le centre du cluster
-    d=0 #initialisation debit cluster 
-    count=0
-    nb_pts_drop=0
-    while(count<len(liste_copie)):
-            d=0
-            print(count)
-            print(len(liste_copie))
-            if dist(0,count,liste_copie)<45 :
-                print("true")
-                d=d+liste_copie[count][2]
-                if (d<debit_MAX):
-                    liste_points[count+nb_pts_drop][3]=num_cluster
-                    liste_copie[count][3]=num_cluster
-                    clusters.append(liste_copie[count])
-                    indices_drop.append(count)
-                    nb_pts_drop+=1
-            for k in range(len(indices_drop)):
-                del liste_copie[indices_drop[k]]
-            count = count + 1
-            indices_drop.clear()
-            num_cluster+=1
-"""
-
-print(clusters)
-print("nb_cluster : ", nb_clusters)
-print("Nombre de clusters ",len(clusters))
-df = pd.DataFrame(clusters).T
-df.to_csv("output.csv")
+#df = pd.DataFrame(clusters).T
+#df.to_csv("output.csv")
